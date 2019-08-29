b5344c8ba61ffef499287472662e63dc2732141b
Alaa Mohammed <alaa4455@gmail.com>
2019-08-29 19:53:18 +0300

    creating the repo

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+$("body").niceScroll({
+  cursorcolor:"rgba(0, 76, 68, 0.76)",
+  cursorwidth:"9px"
+});
+
+var scroll = new SmoothScroll(
+  'a[href*="#"]',
+  
+                             
+                             );
